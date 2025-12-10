e6faa8b91ee8cb2abe04902019aa67b2270721b9
Music195 <minthetkhie195@gmail.com>
2025-12-10 22:59:46 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Student of Shizuoka University.", "Learner.", "Seeker."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    startDelay: 800,
+    backDelay: 2000,
+    loop: true
+});
